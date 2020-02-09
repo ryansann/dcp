393a400a2ba044b321452cf64dab1126ee7b1a7f
Ryan Sanna <ryansann@umich.edu>
2020-02-09 12:52:56 -0700

    fix readme, update comments

--- 423/go/closure.go
+++ 423/go/closure.go
@@ -6,21 +6,23 @@ type closure [][]bool
 
 // transitiveClosure returns the transitive closure for the passed in graph g,
 // where g is represented as an adjacency list.
+// runtime: O(v^2) where v is # of verticies
 func transitiveClosure(g graph) closure {
 	if g == nil {
 		return nil
 	}
 
 	var c closure
-	for i := range g {
-		c = append(c, dfs(g, i, make([]bool, len(g))))
+	for i := range g { // perform dfs for each vertex
+		c = append(c, dfs(g, i, make([]bool, len(g)))) // v * (v + e) = v^2 + ve = O(v^2)
 	}
 
 	return c
 }
 
 // dfs performs a depth first search on g from vertex.
-// it returns a slice that indicates which vertexes are reachable from vertex.
+// it returns a slice that indicates which verticies are reachable from vertex.
+// runtime: O(v + e) where v is # of verticies and e is # of edges.
 func dfs(g graph, vertex int, visited []bool) []bool {
 	visited[vertex] = true
 
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Requirements:
 - Node.js
 - Golang
 
-To run all tests, use: `make`
-To run all javascript tests, use: `make run-js`
-To run all go tests, use: `make run-go`
+To run all tests, use: `make`  
+To run all javascript tests, use: `make run-js`  
+To run all go tests, use: `make run-go`  
