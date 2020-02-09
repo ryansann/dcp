ea45aa514d8db55b6c25132e457ab9a1a3f05463
Ryan Sanna <ryansann@umich.edu>
2020-02-09 15:23:52 -0700

    restructure solution

--- 446/go/power.go
+++ 446/go/power.go
@@ -5,17 +5,21 @@ import "math/bits"
 // isPowerOfFour detects whether or not n is a power of 4
 // it runs in constant time
 func isPowerOfFour(n uint32) bool {
-	if n != 0 {
-		if (n & (n - 1)) == 0 { // only 1 bit is set
-			// count 0 bits before the 1
-			var count int
-			for ; n > 1; count++ {
-				n >>= 1
-			}
-			return count%2 == 0 // return true if even number of 0 bits before 1
-		}
+	if n == 0 { // 0 is not a power of 4
+		return false
 	}
-	return false
+
+	if (n & (n - 1)) != 0 { // more than 1 bit is set
+		return false
+	}
+
+	// count 0 bits before the 1
+	var count int
+	for ; n > 1; count++ { // when the set bit is at position 0, we've counted all trailing unset bits
+		n >>= 1 // right shift one bit
+	}
+
+	return count%2 == 0 // return true if even number of 0 bits before 1
 }
 
 // isPowerOfFourEasy outsources bitwise operations to the math/bits package
